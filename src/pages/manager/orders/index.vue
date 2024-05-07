e4bbcaf7f2c9e4c1dda07d9d04624058261ee38b
lukser69 <russergey2001@mail.ru>
2024-05-07 09:01:34 +0300

    feat: отображаются заказы + переход к заказу

--- src/pages/manager/orders/index.vue
+++ src/pages/manager/orders/index.vue
@@ -3,11 +3,14 @@
     <template #fixed>
       <OrdersFilters />
     </template>
-    <template #default />
+    <template #default>
+      <Orders />
+    </template>
   </FixedLeftColumn>
 </template>
 
 <script setup lang="ts">
 import { FixedLeftColumn } from "@/shared/ui/templates";
 import { OrdersFilters } from "@/widgets/orders-filters";
+import { Orders } from "@/widgets/orders";
 </script>
---
+++ src/widgets/orders/orders.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul v-if="orders?.length" class="orders-list">
+    <li v-for="order in orders" :key="order.id" class="orders-list__item">
+      <StatusCard @click="goToOrder(order.id)">{{ order.id }}</StatusCard>
+    </li>
+  </ul>
+
+  <Empty v-else>Заказов еще нет</Empty>
+</template>
+
+<script setup lang="ts">
+import { StatusCard } from "@/shared/ui/status-card";
+import { Empty } from "@/shared/ui/empty";
+import { internalAPIFetch } from "@/shared/api/internal/instance";
+
+import type { IOrder } from "./types";
+
+const router = useRouter();
+
+const orders = ref<null | IOrder[]>(null);
+
+const getOrders = async () => {
+  try {
+    interface IResponse {
+      response: {
+        data: {
+          orders: IOrder[];
+        };
+      };
+    }
+
+    const response: IResponse = await internalAPIFetch("/orders.getTest");
+    orders.value = response?.response?.data?.orders;
+    // console.log(response);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+onMounted(getOrders);
+
+const goToOrder = (orderId: string) => {
+  const currentPath = router.currentRoute.value.path;
+  router.push(currentPath + `/${orderId}`);
+};
+</script>
+
+<style lang="scss">
+@import "~/shared/assets/styles/components/orders/orders-list";
+</style>
